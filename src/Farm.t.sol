67b7013d15ef6af50d421cca9f931de4d6afde50
Nazar Duchak <nazar@iovlabs.org>
2023-03-23 12:32:28 +0200

    chore: add base

---
+++ src/Farm.t.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.8.14;
+
+import {Test} from "forge-std/Test.sol";
+
+// import "ds-token/token.sol";
+
+contract FarmTest is Test {
+    uint256 a = 1;
+}
+
+// contract TestToken is DSToken {
+//     constructor(string memory symbol_, uint8 decimals_) public DSToken(symbol_) {
+//         decimals = decimals_;
+//     }
+// }
